21d34d54c4bfefb63de9b6047cb1eb764f43f121
thejaylajones <janeissaj@gmail.com>
2025-09-08 21:36:55 -0400

    created a new and clean api with all dependencies

---
+++ src/main/java/com/maxxenergy/MaxxEnergyAPI/MaxxEnergyApiApplication.java
@@ -0,0 +1,13 @@
+package com.maxxenergy.MaxxEnergyAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxxEnergyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaxxEnergyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maxxenergy/MaxxEnergyAPI/MaxxEnergyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxxenergy.MaxxEnergyAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaxxEnergyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
